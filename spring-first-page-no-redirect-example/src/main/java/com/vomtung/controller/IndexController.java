75b062041a89abdb1cc3b4ddd7218ac133e2ab63
hbthuy-tma <vominhtung@tma.com.vn>
2014-09-16 11:23:10 +0700

    This is example for init the first page without create the  a redirect file

---
+++ spring-first-page-no-redirect-example/src/main/java/com/vomtung/controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.vomtung.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/index", method = RequestMethod.GET)
+public class IndexController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Locale locale, Model model) {
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "index";
+	}
+}
